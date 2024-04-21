cc757536115e65a5a9d0602fef49b33dcab36c43
Yiya Wu <126053960+Yiya-Wu@users.noreply.github.com>
2024-04-21 22:27:24 +0800

    Update Paiwan_BibleTextExtractor.py

--- Paiwan_BibleTextExtractor.py
+++ Paiwan_BibleTextExtractor.py
@@ -11,14 +11,14 @@ def main():
         html_contents = file.read()
         print(html_contents)
     
-        soup = BeautifulSoup(html_contents, "html.parser")
+    soup = BeautifulSoup(html_contents, "html.parser")
 
         # Example: Find all <a> tags with class "some-class"
-        links = soup.find_all("a", class_="some-class")
+        # links = soup.find_all("a", class_="some-class")
 
         # Iterate through the found links and do something with them
-        for link in links:
-            print(link.get("href"))
+        # for link in links:
+            # print(link.get("href"))
     return None
     
 if __name__ == "__main__":
