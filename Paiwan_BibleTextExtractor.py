35eeecac0a19f5ffae4da4bae83ee9a1d76a3a6a
Yiya Wu <126053960+Yiya-Wu@users.noreply.github.com>
2024-04-29 18:18:39 +0800

    Update Paiwan_BibleTextExtractor.py

--- Paiwan_BibleTextExtractor.py
+++ Paiwan_BibleTextExtractor.py
@@ -3,24 +3,16 @@
 
 from bs4 import BeautifulSoup
 
-def main():
-
-    # Open the file in read mode ('r')
-    with open(file_path, 'r') as file:
-        # Read the contents of the file
-        html_contents = file.read()
-        print(html_contents)
-    
-    soup = BeautifulSoup(html_contents, "html.parser")
-
-        # Example: Find all <a> tags with class "some-class"
-        # links = soup.find_all("a", class_="some-class")
-
-        # Iterate through the found links and do something with them
-        # for link in links:
-            # print(link.get("href"))
-    return None
+def main(inputSTR):
+    soup = BeautifulSoup(inputSTR, "html.parser")
+    result = str(soup.get_text())
+    print(result, '===')    
+    a = result.replace('(暫無內容)', '')
+    print(a)
     
 if __name__ == "__main__":
-    file_path = "/Users/yiyawu/Documents/yiya/00.txt"
-    main()
+    file_path = "/Users/yiyawu/Documents/yiya/Paiwan_marku.txt"
+    with open(file_path, 'r') as file:
+        html_contents = file.read()
+        
+        main(html_contents)
